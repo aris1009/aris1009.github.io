dc6af19e688d0e0347edc40a69d37f6dc242d5ff
Aris Konstantoulas <25184469+aris1009@users.noreply.github.com>
2025-07-21 01:24:25 +0300

    feat: initial commit

---
+++ src/_data/meta.js
@@ -0,0 +1,34 @@
+module.exports = {
+  //
+  // Website Meta
+  //  
+  language: "en-US",
+  lang: "en",
+  siteName: "Security & Tech Blog",
+  shortName: "SecTech Blog",
+  motto: "Simplicity is the ultimate sophistication.",
+  authorName: "Aris Konstantoulas",
+  siteDescription: "A blog about cybersecurity, technology, and digital privacy",
+  sitekeyword: "cybersecurity, security, technology, privacy, hacking, malware",
+  creationDate: "21/07/2025",
+  url: process.env.URL || "https://www.marcomicale.eu", // change this url
+  robots: "index, follow",
+  //
+  // Color App
+  //
+  themeColor: "#000000",
+  backgroundColor: "#000000",
+  //
+  // Contact me
+  //
+  mail: "tiny-fade-blip@duck.com",
+  telephone: "",
+  //
+  // Social Network
+  //
+  twitterUser: "", 
+  facebookUser: "",
+  githubUser: "aris1009",
+  telegramkUser: "",
+  linkedinkUser: "aris-konstantoulas",
+};
