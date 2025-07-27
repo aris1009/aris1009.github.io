b1a4781c08868bb1063066b242945b0d715285fb
Aris Konstantoulas <25184469+aris1009@users.noreply.github.com>
2025-07-28 00:07:32 +0300

    feat(blog): claude code (#9)

--- src/lib/collections.js
+++ src/lib/collections.js
@@ -1,15 +1,15 @@
 const { BLOG_GLOBS } = require("./constants");
 
 function postsEn_us(collectionApi) {
-  return collectionApi.getFilteredByGlob(BLOG_GLOBS["en-us"]).filter(post => !post.data.draft);
+  return collectionApi.getFilteredByGlob(BLOG_GLOBS["en-us"]).filter(post => !post.data.draft).reverse();
 }
 
 function postsEl(collectionApi) {
-  return collectionApi.getFilteredByGlob(BLOG_GLOBS.el).filter(post => !post.data.draft);
+  return collectionApi.getFilteredByGlob(BLOG_GLOBS.el).filter(post => !post.data.draft).reverse();
 }
 
 function postsTr(collectionApi) {
-  return collectionApi.getFilteredByGlob(BLOG_GLOBS.tr).filter(post => !post.data.draft);
+  return collectionApi.getFilteredByGlob(BLOG_GLOBS.tr).filter(post => !post.data.draft).reverse();
 }
 
 function allPosts(collectionApi) {
