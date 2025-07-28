4fbff8e78afe02399d9cb7597c3e59862803571f
Aris Konstantoulas <25184469+aris1009@users.noreply.github.com>
2025-07-28 08:33:19 +0300

    fix: prism css style link

--- .eleventy.js
+++ .eleventy.js
@@ -27,7 +27,6 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("src/_static");
   eleventyConfig.addPassthroughCopy({ "src/_static/img": "img" });
-  eleventyConfig.addPassthroughCopy({ "src/_tailwindCSS/prism.css": "_static/css/prism.css" });
 
   eleventyConfig.addFilter("readableDate", filters.readableDate);
   eleventyConfig.addFilter("htmlDateString", filters.htmlDateString);
